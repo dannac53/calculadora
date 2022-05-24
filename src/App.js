3d8bd493d1e8289b7396456de70401402574b8a6
dannac53 <danacontreras53@gmail.com>
2022-05-24 15:50:31 -0500

    ahi te va

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div >
+        <h1>INTEGRALES DEFINIDAS</h1>
+      </div>
+      <div className="row">
+        <div className="col-12 row">
+          <label className="col-3">T.SIMPSON 1/3-NEWTON</label>
+          <label className="col-2">JORGE BOOLE</label>
+          <label className="col-3">T.SIMPSON 3/8-NEWTON</label>
+          <label className="col-2">SIMPSON ABIERTO</label>
+          <label className="col-2">TRAPEZOIDAL</label>
+        </div>
+        <div></div>
+        <div className="col-12">
+          <input></input>
+          <button type="submit" className="">Hallar solucion</button>
+          <button type="submit" className="">Limpiar campos</button>
+        </div>
+      </div>
     </div>
   );
 }
